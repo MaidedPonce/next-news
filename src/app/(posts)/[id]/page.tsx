cf4a3d6685c3a03dcecce12ea38bb2e91ba4a3e5
MaidedPonce <maided.h.p90@gmail.com>
2024-07-07 21:45:23 -0600

    Adding tags

--- src/app/(posts)/[id]/page.tsx
+++ src/app/(posts)/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Post from 'app/components/Blog/Posts/Post'
 import { getPostById } from 'app/services/blog/get-by-id'
 import { PostInterface } from 'app/services/blog/types'
+import { Metadata } from 'next'
 
 interface PostProps {
   params: {
@@ -8,8 +9,23 @@ interface PostProps {
   }
 }
 
-const PostPage = async ({ params: { id } }: PostProps) => {
+const getPostByIdFn = async (id: string) => {
   const post: PostInterface | undefined = await getPostById(id)
+  return post
+}
+
+export async function generateMedata({
+  params: { id },
+}: PostProps): Promise<Metadata> {
+  const post = await getPostByIdFn(id)
+  return {
+    title: post?.title?.rendered,
+    description: post?.excerpt?.toString() ?? '',
+  }
+}
+
+const PostPage = async ({ params: { id } }: PostProps) => {
+  const post = await getPostByIdFn(id)
   return <Post post={post} />
 }
 
